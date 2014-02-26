b136d3830692d0a554c7c3fc0dd0d980b22b88d8
/herve Senot <hsenot@gmail.com>
2014-02-26 02:45:27 +0000

    Infinite loop with a 10 second resolution

--- control.py
+++ control.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python
 
 import os
+import time
 import urllib, urllib2
 import json
 
 url = "http://www.temporary-agency.com/ws/ws_get_metrics.php"
+previous_sector=1
+while True:
+	time.sleep(10)
 
-response = urllib2.urlopen(url)
-data = json.load(response)   
-print data
+	response = urllib2.urlopen(url)
+	data = json.load(response)   
+	print data
 
-last_mn=int(str(data['rows'][0]['last_mn']))
-last_5mn=int(str(data['rows'][0]['last_5mn']))
-last_h=int(str(data['rows'][0]['last_hour']))
-last_4h=int(str(data['rows'][0]['last_4hour']))
-print last_mn,last_5mn,last_h,last_4h
+	last_10s=int(str(data['rows'][0]['last_10s']))
+	last_mn=int(str(data['rows'][0]['last_mn']))
+	last_5mn=int(str(data['rows'][0]['last_5mn']))
+	last_h=int(str(data['rows'][0]['last_hour']))
+	print last_10s,last_mn,last_5mn,last_h
 
-sector=1
-if last_mn>0:
-	sector=4
-elif last_5mn>0:
-	sector=3
-elif last_h>0:
-	sector=2
-print "Going to sector ",sector
+	sector=1
+	if last_10s>0:
+		sector=4
+	elif last_mn>0:
+		sector=3
+	elif last_5mn>0:
+		sector=2
 
-# Control the servo based on this information
-os.system("/home/pi/Projects/arduino-controller/servocontrol.py "+str(sector))
+	if sector <> previous_sector:
+		print "Going to sector ",sector
+		previous_sector=sector
+
+		# Control the servo based on this information
+		os.system("/home/pi/Projects/arduino-controller/servocontrol.py "+str(sector))
 
 
 
