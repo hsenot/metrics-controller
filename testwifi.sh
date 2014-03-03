a4d9acdaf767a8d602193c2418ed03f5837ecbbc
/herve Senot <hsenot@gmail.com>
2014-03-03 07:02:30 +0000

    Adding script to test wifi is up

---
+++ testwifi.sh
@@ -0,0 +1,13 @@
+TESTIP=192.168.0.1
+
+ping -c4 ${TESTIP} > /dev/null
+
+if [ $? != 0 ]
+then
+    logger -t $0 "WiFi seems down, restarting"
+    sudo /sbin/ifdown --force wlan0
+    sleep 10
+    sudo /sbin/ifup wlan0
+else
+    logger -t $0 "WiFi seems up."
+fi
